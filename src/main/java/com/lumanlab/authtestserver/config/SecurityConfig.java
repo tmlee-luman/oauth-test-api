0404908330e1d7111ea560a546981a954131b434
tmlee <tmlee@lumanlab.com>
2022-06-22 11:05:36 +0900

    필터, 예외 entrypoint 추가

--- src/main/java/com/lumanlab/authtestserver/config/SecurityConfig.java
+++ src/main/java/com/lumanlab/authtestserver/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.lumanlab.authtestserver.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lumanlab.authtestserver.infrastructure.jwt.JwtTokenHelper;
+import com.lumanlab.authtestserver.security.entrypoint.JwtAuthenticationEntryPoint;
+import com.lumanlab.authtestserver.security.filter.JwtAuthenticationFilter;
 import com.lumanlab.authtestserver.security.url.BasicAuthenticationExclusionV1Uri;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    private final JwtTokenHelper jwtTokenHelper;
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,7 +48,12 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, BasicAuthenticationExclusionV1Uri.getUris())
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(userDetailsService, jwtTokenHelper), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint(objectMapper));
+
 
         return http.build();
     }
@@ -51,4 +70,5 @@ public class SecurityConfig {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return urlBasedCorsConfigurationSource;
     }
+
 }
